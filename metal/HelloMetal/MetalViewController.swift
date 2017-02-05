2e067fa8989e6862e618b98af4718d89a16677f1
hironand <dev.hironaga@gmail.com>
2017-02-05 15:46:20 +0900

    fix: to MTKView but running error

--- metal/HelloMetal/MetalViewController.swift
+++ metal/HelloMetal/MetalViewController.swift
@@ -22,108 +22,80 @@
 
 import UIKit
 import Metal
+import MetalKit
 import QuartzCore
 import simd
 
 protocol MetalViewControllerDelegate : class {
-  func updateLogic(_ timeSinceLastUpdate:CFTimeInterval)
-  func renderObjects(_ drawable:CAMetalDrawable)
+    func updateLogic(_ timeSinceLastUpdate:CFTimeInterval)
+    func renderObjects(_ drawable:CAMetalDrawable)
 }
 
 class MetalViewController: UIViewController {
+    
+    var device: MTLDevice! = nil
+    var pipelineState: MTLRenderPipelineState! = nil
+    var commandQueue: MTLCommandQueue! = nil
+    var projectionMatrix: float4x4!
+    
+    @IBOutlet var mtkView: MTKView!
+    
+    weak var metalViewControllerDelegate:MetalViewControllerDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        projectionMatrix = float4x4.makePerspectiveViewAngle(float4x4.degrees(toRad: 85.0), aspectRatio: Float(self.view.bounds.size.width / self.view.bounds.size.height), nearZ: 0.01, farZ: 100.0)
+        
+        mtkView.device = device
+        
+        device = MTLCreateSystemDefaultDevice()
+        
+        commandQueue = device.makeCommandQueue()
+        
+        let defaultLibrary = device.newDefaultLibrary()
+        let fragmentProgram = defaultLibrary!.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary!.makeFunction(name: "basic_vertex")
+        
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        pipelineStateDescriptor.colorAttachments[0].isBlendingEnabled = true
+        pipelineStateDescriptor.colorAttachments[0].rgbBlendOperation = MTLBlendOperation.add;
+        pipelineStateDescriptor.colorAttachments[0].alphaBlendOperation = MTLBlendOperation.add;
+        pipelineStateDescriptor.colorAttachments[0].sourceRGBBlendFactor = MTLBlendFactor.one;
+        pipelineStateDescriptor.colorAttachments[0].sourceAlphaBlendFactor = MTLBlendFactor.one;
+        pipelineStateDescriptor.colorAttachments[0].destinationRGBBlendFactor = MTLBlendFactor.oneMinusSourceAlpha;
+        pipelineStateDescriptor.colorAttachments[0].destinationAlphaBlendFactor = MTLBlendFactor.oneMinusSourceAlpha;
+        
+        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+        
+        mtkView.delegate = self
+        mtkView.preferredFramesPerSecond = 60
+        mtkView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+    }
+    
+    func render(_ drawable: CAMetalDrawable?) {
+        guard let drawable = drawable else { return }
+        self.metalViewControllerDelegate?.renderObjects(drawable)
+    }
+    
+}
 
-  var device: MTLDevice! = nil
-  var metalLayer: CAMetalLayer! = nil
-  var pipelineState: MTLRenderPipelineState! = nil
-  var commandQueue: MTLCommandQueue! = nil
-  var timer: CADisplayLink! = nil
-  var projectionMatrix: float4x4!
-  var lastFrameTimestamp: CFTimeInterval = 0.0
-  
-  weak var metalViewControllerDelegate:MetalViewControllerDelegate?
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+// MARK: - MTKViewDelegate
+extension MetalViewController: MTKViewDelegate {
     
-    projectionMatrix = float4x4.makePerspectiveViewAngle(float4x4.degrees(toRad: 85.0), aspectRatio: Float(self.view.bounds.size.width / self.view.bounds.size.height), nearZ: 0.01, farZ: 100.0)
-    
-    
-    device = MTLCreateSystemDefaultDevice()
-    metalLayer = CAMetalLayer()
-    metalLayer.device = device
-    metalLayer.pixelFormat = .bgra8Unorm
-    metalLayer.framebufferOnly = true
-    view.layer.addSublayer(metalLayer)
-    
-    
-    commandQueue = device.makeCommandQueue()
-    
-    let defaultLibrary = device.newDefaultLibrary()
-    let fragmentProgram = defaultLibrary!.makeFunction(name: "basic_fragment")
-    let vertexProgram = defaultLibrary!.makeFunction(name: "basic_vertex")
-    
-    
-    let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
-    pipelineStateDescriptor.vertexFunction = vertexProgram
-    pipelineStateDescriptor.fragmentFunction = fragmentProgram
-    pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-    pipelineStateDescriptor.colorAttachments[0].isBlendingEnabled = true
-    pipelineStateDescriptor.colorAttachments[0].rgbBlendOperation = MTLBlendOperation.add;
-    pipelineStateDescriptor.colorAttachments[0].alphaBlendOperation = MTLBlendOperation.add;
-    pipelineStateDescriptor.colorAttachments[0].sourceRGBBlendFactor = MTLBlendFactor.one;
-    pipelineStateDescriptor.colorAttachments[0].sourceAlphaBlendFactor = MTLBlendFactor.one;
-    pipelineStateDescriptor.colorAttachments[0].destinationRGBBlendFactor = MTLBlendFactor.oneMinusSourceAlpha;
-    pipelineStateDescriptor.colorAttachments[0].destinationAlphaBlendFactor = MTLBlendFactor.oneMinusSourceAlpha;
-    
-    pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
-    
-    timer = CADisplayLink(target: self, selector: #selector(MetalViewController.newFrame(_:)))
-    timer.add(to: RunLoop.main, forMode: RunLoopMode.defaultRunLoopMode)
-    
-  }
-  
-  //1
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-    
-    if let window = view.window {
-      let scale = window.screen.nativeScale
-      let layerSize = view.bounds.size
-      //2
-      view.contentScaleFactor = scale
-      metalLayer.frame = CGRect(x: 0, y: 0, width: layerSize.width, height: layerSize.height)
-      metalLayer.drawableSize = CGSize(width: layerSize.width * scale, height: layerSize.height * scale)
-    }
-    projectionMatrix = float4x4.makePerspectiveViewAngle(float4x4.degrees(toRad: 85.0), aspectRatio: Float(self.view.bounds.size.width / self.view.bounds.size.height), nearZ: 0.01, farZ: 100.0)
-  }
-
-  func render() {
-    if let drawable = metalLayer.nextDrawable() {
-      self.metalViewControllerDelegate?.renderObjects(drawable)
-    }
-  }
-  
-  
-  func newFrame(_ displayLink: CADisplayLink) {
-    
-    if lastFrameTimestamp == 0.0 {
-      lastFrameTimestamp = displayLink.timestamp
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        projectionMatrix = float4x4.makePerspectiveViewAngle(float4x4.degrees(toRad: 85.0),
+                                                             aspectRatio: Float(self.view.bounds.size.width / self.view.bounds.size.height),
+                                                             nearZ: 0.01, farZ: 100.0)
     }
     
-    let elapsed:CFTimeInterval = displayLink.timestamp - lastFrameTimestamp
-    lastFrameTimestamp = displayLink.timestamp
-    
-    gameloop(elapsed)
-  }
-  
-  func gameloop(_ timeSinceLastUpdate: CFTimeInterval) {
-    
-    self.metalViewControllerDelegate?.updateLogic(timeSinceLastUpdate)
-    
-    autoreleasepool {
-      self.render()
+    func draw(in view: MTKView) {
+        render(view.currentDrawable)
     }
-  }
-
+    
 }
 
